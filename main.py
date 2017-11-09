5ad54b06d6cc18fdd1d58e9f1ee41dfd76fa12dd
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-11-09 23:52:02 +0330

    Define Paths

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # 2017-11-09
 # ********************
 
+from tkinter import messagebox
+import tkinter as tk
+from tkinter import font as tkfont
+from pathlib import Path
+
+
+# Defining Paths
+img_path = Path("pix\calculator.png").resolve()
+icon_path = Path("pix\icon.ico").resolve()
+

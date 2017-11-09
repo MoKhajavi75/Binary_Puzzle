5a8be1c7bf5d9e5f456e51374121a239bc58d406
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-11-09 23:56:49 +0330

    Add About Frame

--- main.py
+++ main.py
@@ -95,4 +95,15 @@ class HighScores(tk.Frame):
         button.pack()
 
 
+class About(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="About Us", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
 
